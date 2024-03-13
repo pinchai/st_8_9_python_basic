e5b3d2311184012624b2dc3bdca742cab76da583
Pinchai <>
2024-03-13 09:23:34 +0700

    telegram

---
+++ find_max_value.py
@@ -0,0 +1,28 @@
+a = input("Please input value a: ")
+b = input("Please input value b: ")
+
+if a > b:
+    print(f"Max value is a ({a})")
+else:
+    print(f"Max value is b ({b})")
+
+
+#include <iostream>
+#include <string>
+
+# int main() {
+#     std::string a, b;
+#     std::cout << "Please input value a: ";
+#     std::cin >> a;
+#     std::cout << "Please input value b: ";
+#     std::cin >> b;
+#
+#     if (a > b) {
+#         std::cout << "Max value is a (" << a << ")\n";
+#     } else {
+#         std::cout << "Max value is b (" << b << ")\n";
+#     }
+#
+#     return 0;
+# }
+
--- sets.py
+++ sets.py
@@ -1,24 +1,9 @@
-
-std_list = ['ty', 'chan', 'ty', 'ra']
-set_list = set(std_list)
-
-group_name = []
-for unique_name in set_list:
-    count = 0
-    for current in std_list:
-        if current == unique_name:
-            count+=1
-            print(unique_name)
-    group_name.append({
-        'name': unique_name,
-        'count': count
-    })
-
-print(group_name)
-
-
-
-#
-# std_list = {'a', 'b', 'c'}
-# print(std_list[0])
-# # print(type(std_list))
+name = input("Enter name: ")
+gender = input("Enter gender: ")
+age = input("Enter age: ")
+address = input("Enter address: ")
+print("----------------------")
+print(f"Name: {name}")
+print(f"Gender: {gender}")
+print(f"Age: {age}")
+print(f"Address: {address}")
